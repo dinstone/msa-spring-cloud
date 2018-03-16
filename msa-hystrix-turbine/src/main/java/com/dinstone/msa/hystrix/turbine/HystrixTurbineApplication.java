b3e23b3da560fe0865817e6e9ac02fd60f54b81f
dinstone <dinstone@163.com>
2018-03-16 17:58:28 +0800

    add hystrix

--- msa-hystrix-turbine/src/main/java/com/dinstone/msa/hystrix/turbine/HystrixTurbineApplication.java
+++ msa-hystrix-turbine/src/main/java/com/dinstone/msa/hystrix/turbine/HystrixTurbineApplication.java
@@ -1,16 +1,16 @@
-package com.dinstone.msa.hystrix.dashboard;
+package com.dinstone.msa.hystrix.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableHystrixDashboard
-public class HystrixDashboardApplication {
+@EnableTurbine
+public class HystrixTurbineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HystrixDashboardApplication.class, args);
+		SpringApplication.run(HystrixTurbineApplication.class, args);
 	}
 }
