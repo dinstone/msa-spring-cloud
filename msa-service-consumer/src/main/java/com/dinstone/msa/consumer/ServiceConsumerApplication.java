931bb8e715f331f54879957acde1f5c5dc53db01
dinstone <dinstone@163.com>
2018-03-20 11:11:49 +0800

    @LoadBalanced

--- msa-service-consumer/src/main/java/com/dinstone/msa/consumer/ServiceConsumerApplication.java
+++ msa-service-consumer/src/main/java/com/dinstone/msa/consumer/ServiceConsumerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -19,6 +20,7 @@ public class ServiceConsumerApplication {
 	}
 
 	@Bean
+	@LoadBalanced
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
