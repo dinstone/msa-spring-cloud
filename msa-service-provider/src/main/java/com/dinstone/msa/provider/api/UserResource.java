f6e48d360cfe14d183c4b545a4afcc4c3ea04ac4
dinstone <dinstone@163.com>
2018-04-17 19:43:27 +0800

    add log for tracing

--- msa-service-provider/src/main/java/com/dinstone/msa/provider/api/UserResource.java
+++ msa-service-provider/src/main/java/com/dinstone/msa/provider/api/UserResource.java
@@ -2,6 +2,8 @@ package com.dinstone.msa.provider.api;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,16 +17,20 @@ import com.dinstone.msa.provider.service.UserService;
 @RequestMapping("/user")
 public class UserResource {
 
+	private static final Logger LOG = LoggerFactory.getLogger(UserResource.class);
+
 	@Autowired
 	private UserService userService;
 
 	@GetMapping("/get/{uid}")
 	public User get(@PathVariable("uid") int uid) {
+		LOG.info("get user with {}", uid);
 		return userService.findById(uid);
 	}
 
 	@GetMapping("/list")
 	public List<User> list() {
+		LOG.info("list users");
 		return userService.findAll();
 	}
 }
