6f71589e04adeef23bd87d13b6a752ad047af6ec
dinstone <dinstone@dinstonedembp>
2018-05-10 17:11:56 +0800

    measure endpoint

---
+++ msa-service-consumer/src/main/java/com/dinstone/msa/consumer/endpoint/MeasureEndpoint.java
@@ -0,0 +1,29 @@
+package com.dinstone.msa.consumer.endpoint;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.AbstractEndpoint;
+import org.springframework.stereotype.Component;
+
+import com.codahale.metrics.Metric;
+import com.codahale.metrics.SharedMetricRegistries;
+import com.dinstone.measure.config.MetricConfig;
+
+@Component
+public class MeasureEndpoint extends AbstractEndpoint<Map<String, Metric>> {
+
+	@Autowired
+	private MetricConfig metricConfig;
+
+	public MeasureEndpoint() {
+		super("measure");
+	}
+
+	@Override
+	public Map<String, Metric> invoke() {
+		String registryName = metricConfig.getMetricRegistryName();
+		return SharedMetricRegistries.getOrCreate(registryName).getMetrics();
+	}
+
+}
