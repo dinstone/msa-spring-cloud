ced392024abfee88afe8ed2cbd86bf5750872532
dinstone <dinstone@dinstonedembp>
2018-06-07 09:26:52 +0800

    add measure

--- msa-vertx-service/src/main/java/com/dinstone/msa/vertx/VertxServiceApplication.java
+++ msa-vertx-service/src/main/java/com/dinstone/msa/vertx/VertxServiceApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import com.dinstone.measure.starter.EnableMeasure;
 import com.dinstone.vertx.starter.EnableVertxRest;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableVertxRest
+@EnableMeasure
 public class VertxServiceApplication {
 
 	public static void main(String[] args) {
