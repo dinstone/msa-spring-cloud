15220939a4b3a7c5f65569815c0119b8b6b67712
dinstone <dinstone@163.com>
2023-06-21 15:10:23 +0800

    edit sleuth and actutor

--- msa-service-provider/src/main/java/com/dinstone/msa/provider/ServiceProviderApplication.java
+++ msa-service-provider/src/main/java/com/dinstone/msa/provider/ServiceProviderApplication.java
@@ -1,8 +1,13 @@
 package com.dinstone.msa.provider;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -11,4 +16,10 @@ public class ServiceProviderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceProviderApplication.class, args);
 	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> configurer(@Value("${spring.application.name}") String applicationName) {
+		return (registry) -> registry.config().commonTags("application", applicationName);
+	}
+
 }
